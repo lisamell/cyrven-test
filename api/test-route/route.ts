fd1f1bfdcf00f0d3c5a35bf749b1e941e356f413
melisaeminogllu <melisaeminogllu@gmail.com>
2026-08-14 22:59:41 +0300

    Add GET route to fetch users from Supabase

---
+++ api/test-route/route.ts
@@ -0,0 +1,8 @@
+// test-api/route.ts
+import { NextResponse } from "next/server";
+import { supabase } from "@/lib/supabase";
+
+export async function GET() {
+  const { data } = await supabase.from("users").select("*");
+  return NextResponse.json(data);
+}
